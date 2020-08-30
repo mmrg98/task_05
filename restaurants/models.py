d7902f8fd97dd7f31cc32fc5725db68ce55b0f99
mmrg98 <mariam.elgaria98@gmail.com>
2020-08-30 15:25:48 +0300

    Test task 5

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Restaurant(models.Model):
+    id=models.IntegerField(primary_key=True)
     name = models.CharField(max_length=120)
     description = models.TextField()
     opening_time = models.TimeField()
     closing_time = models.TimeField()
+    def __str__(self):
+        return self.name
